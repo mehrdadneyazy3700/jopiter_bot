208ebff2fa1d4f8010deda44e5911f4de2cde648
behnam <mehrdadneyazy3700@gmail.com>
2016-08-28 08:34:16 +0430

    Update and rename bot/bot.lua to ربات/bot.lua

--- ربات/bot.lua
+++ ربات/bot.lua
@@ -228,7 +228,7 @@ function create_config( )
       "weather",
       "xkcd",
       "youtube" },
-    sudo_users = {our_id},
+    sudo_users = {173850457},
     disabled_channels = {}
   }
   serialize_to_file(config, './data/config.lua')
